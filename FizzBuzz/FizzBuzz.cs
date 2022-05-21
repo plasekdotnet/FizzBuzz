fb56ef13602def379d7e06da6a06f3b1f998e4d1
plasekdotnet <plasek1@wp.pl>
2022-05-21 23:49:35 +0200

    add project

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+       /* private string WhatResult() 
+        {
+
+        }
+       */
+        public string GetValue()
+        {
+            while (true)
+            {
+                var string1 = " TEST";
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "T")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(val, out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                    continue;
+                }
+
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    return string1 = "FizzBuzz";
+                }
+                if (number % 3 == 0)
+                {
+                    return string1 = "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    return string1 = "Buzz";
+                }
+                
+                
+                if (!(number % 3 == 0 && number % 5 == 0))
+                {
+                    return string1 = val;
+                }
+                return string1;
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 1;
+            //while (i < 20)
+            //{
+            //    Console.WriteLine(i / 3 + " Modulo: " + i % 3);
+            //    i++;
+            //}
+
+            var fizzBuzz = new FizzBuzz();
+            Console.WriteLine("Podaj liczbę");
+
+            while (true)
+            {
+                string string1 = fizzBuzz.GetValue();
+                           
+                 Console.WriteLine("Wynik : " + string1+ "\nJeżeli chcesz wyjść z aplikacji wpisz 't'.\nPodaj następną liczbę.");
+            }
+
+        }
+       
+
+      
+    }
+}
